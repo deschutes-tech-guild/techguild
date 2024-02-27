374f5c4f02b8351ac1e56662529448036ff317eb
Tyler Hardison <tylerh@evercharge.com>
2024-02-27 10:54:15 -0800

    chore: force a deployment

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,6 +2,5 @@ export const siteConfig = {
   title: 'Bend DevOps',
   description:
     'The homepage for the Bend DevOps community. We are a group of developers and operations engineers in Bend, Oregon. We meet monthly to discuss DevOps topics and share knowledge.',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://benddevops.org',
 };
