272f1a6ec75ebece6e6a3fadd3a383badf3841ac
Nathan <nslee333@gmail.com>
2024-02-28 18:05:59 -0800

    fix: swapped out fira sans as the default font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Inter', ...defaultTheme.fontFamily.sans],
+        primary: ['Fira Sans', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: {
