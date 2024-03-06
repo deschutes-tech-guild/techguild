4bb05769af20d3ced088ab6f05a89b11a313b824
Nathan <nslee333@gmail.com>
2024-03-06 11:29:47 -0800

    test: trying to fix aspect ratio

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <div className='w-full flex flex-row-reverse justify-stretch'>
         <div className='w-1/3 mx-auto flex justify-end'>
           <Image
-            className='max-w-[115px] max-h-[113px]'
+            className='max-h-[115px] max-w-[113px]  h-[115px] w-[113px]'
             src='/images/bdo-logo.webp'
             alt='Bend DevOps Logo'
             width={150}
