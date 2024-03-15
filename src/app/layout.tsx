fb3ef693ce3c4bc5dda0e5558aa8772fc6496f68
Nathan <nslee333@gmail.com>
2024-03-15 11:05:41 -0700

    fix: added lang to HTML attribute

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang='en-US'>
       <body>
         {children}
         <SpeedInsights />
