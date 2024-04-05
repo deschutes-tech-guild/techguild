44352e5fd27aa0a54d00225b8890d0f33ef59ef4
Nathan <nslee333@gmail.com>
2024-04-04 23:13:23 -0700

    chore: cleaning up my debug sillyness

--- snyk-script.sh
+++ snyk-script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# WIP! Haven't tested this script yet.
-
-# ! Add debug logs to make sure it works
 
 # This script is for automating pulling down a Snyk PR
 # and updating pnpm-lock.yaml since Snyk bot dosen't quite support that yet.
@@ -10,27 +7,21 @@
 
 echo "hello world"
 
+# Grab the latest Snyk branch from a list of remotes.
 snyk_branch=$( git branch -r --sort=-authordate | grep "snyk" | head -1 | cut -c 10-)
-read -rp "Press enter to continue..."
 
-echo "$snyk_branch" "SNYK"
-read -rp "Press enter to continue..."
+echo "$snyk_branch"
 
 git switch "$snyk_branch"
-read -rp "Press enter to continue..."
 
 pnpm install
-read -rp "Press enter to continue..."
 
 git commit -am "chore: pnpm install"
-read -rp "Press enter to continue..."
 
 git push
-read -rp "Press enter to continue..."
 
 git checkout main
-read -rp "Press enter to continue..."
 
-# git branch -d "$snyk_branch"
+git branch -d "$snyk_branch"
 
 echo "All done :)"
